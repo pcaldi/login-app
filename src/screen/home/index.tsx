c2822d74253a4922213de68befd8d3a450630854
pcaldi <pcaldi@gmail.com>
2024-02-03 23:18:49 -0300

    feat: Validar formulário no aplicativo

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -7,6 +7,8 @@ import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 
+import api from '@/services/api';
+
 
 export function Home() {
   // Armazenar informações do usuário
@@ -26,28 +28,37 @@ export function Home() {
     navigation.navigate('recoverPassword');
   }
 
-
   async function handleLoginSubmit() {
-    const req = await fetch('http://192.168.0.102:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
+
+    if (!validate()) return;
+
+    await api.post('/login', {
+      email,
+      password,
+    }).then((response) => {
+      //console.log(response.data.message.toString());
+      Alert.alert('Sucesso', response.data.message.toString());
+    }).catch((error) => {
+      //console.log(error.response.data.message.toString());
+      Alert.alert('Ops', error.response.data.message.toString());
+      if (error.response) {
+        Alert.alert('Ops', error.response.data.message.toString());
+      } else {
+        Alert.alert('Ops', 'Tente Novamente mais tarde.');
+      }
     });
+  }
 
-    const result = await req.json();
-    //console.log(result)
-
-    if (result.error) {
-      Alert.alert("", result.message);
-    } else {
-      Alert.alert("", result.message);
+  const validate = () => {
+    if (!email) {
+      Alert.alert('Ops', 'Necessário preenchimento do campo usuário.');
+      return false;
     }
-
+    if (!password) {
+      Alert.alert('Ops', 'Necessário preenchimento do campo senha.');
+      return false;
+    }
+    return true
   }
 
   return (
