cd322d2a4f871b5aeb9ec889ceafd4919b336aa1
pcaldi <pcaldi@gmail.com>
2024-02-07 14:10:24 -0300

    feat: add icon name no componente

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,24 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import { styles } from "./styles";
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
+// Para pegar/tipar todos o item que tem na biblioteca
+export type IconNameType = keyof typeof MaterialCommunityIcons.glyphMap
 
-type HeaderProps = {
+interface HeaderProps extends TouchableOpacityProps {
   title: string;
-  onPress: () => void;
+  onPress?: () => void;
+  IconName: IconNameType;
 }
 
-export function Header({ title, onPress }: HeaderProps) {
+export function Header({ title, onPress, IconName, ...rest }: HeaderProps) {
   return (
 
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.icon}>
-        <TouchableOpacity style={styles.button} onPress={onPress} activeOpacity={0.7}>
-          <MaterialCommunityIcons name="logout" color="#F5F5F5" size={28} />
+        <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
+          <MaterialCommunityIcons name={IconName} color="#F5F5F5" size={28} />
         </TouchableOpacity>
       </View>
     </View>
