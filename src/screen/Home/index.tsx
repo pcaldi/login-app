889857b92f6b2675e9e908577f275e17c54f8a43
pcaldi <pcaldi@gmail.com>
2024-02-10 12:06:34 -0300

    feat: home

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,93 +1,16 @@
-import { useEffect, useState } from 'react';
 
-// Navegar entre telas
-import { useNavigation } from '@react-navigation/native';
-
-// Incluir AsyncStorage para armazenar dados no dispositivo
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import { getValToken } from '@/services/auth';
-
+// Incluir os componentes utilizado para estruturar o conteúdo
 import { Text, View } from 'react-native';
-
-// Estilo do componente
 import { styles } from './styles';
-
-// Componentes
 import { Header } from '@/components/Header';
-import Toast from 'react-native-toast-message';
 
-
+// Criar e exportar a função com a tela home
 export function Home() {
-  // Armazenar informações nos estados/State
-  const [token, setToken] = useState<string | null>('');
-  const [name, setName] = useState<string | null>('');
-  const [email, setEmail] = useState<string | null>('');
 
-  // Navegar entre as telas
-  const navigation = useNavigation();
-
-  // Função que retorna para a tela de login
-  function handleLoginScreen() {
-    navigation.navigate('login');
-  }
-
-  // Recuperar o valor que esta no AsyncStorage
-  const getValue = async () => {
-
-    try {
-      const valToken = await getValToken();
-
-      if (valToken === null) {
-        Toast.show({
-          type: 'error',
-          text1: 'Ops',
-          text2: 'Erro: Necessário realizar o login para acessar a página.',
-          text2Style: {
-            fontSize: 11
-          }
-        });
-        // Redirecionar para tela de login
-        navigation.navigate('login');
-      } else {
-
-        const valName = await AsyncStorage.getItem('@name');
-        const valEmail = await AsyncStorage.getItem('@email');
-        setToken(valToken);
-        setName(valName);
-        setEmail(valEmail);
-      }
-
-    } catch (error) {
-      Toast.show({
-        type: 'error',
-        text1: 'Ops',
-        text2: 'Erro: Necessário realizar o login para acessar a página.',
-        text2Style: {
-          fontSize: 11
-        }
-      });
-      // Redirecionar para tela de login
-      navigation.navigate('login');
-    }
-
-  }
-
-  useEffect(() => {
-    getValue();
-  }, [])
 
   return (
     <View style={styles.container}>
-      <Header title='Home' IconName='logout' onPress={handleLoginScreen} />
-
-
-      <View style={styles.content}>
-        <Text style={styles.text}>Name: {name}</Text>
-        <Text style={styles.text}>E-mail: {email}</Text>
-        <Text style={styles.text}>@Token: {token}</Text>
-      </View>
+      <Header title='Home' IconName='account-clock' />
     </View>
-  );
+  )
 }
-
--- src/screen/Home/styles.ts
+++ src/screen/Home/styles.ts
@@ -2,24 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#10101c',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  content: {
     flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#10101c',
   },
-  text: {
-    fontSize: 16,
-    color: '#FFFFFF',
-    alignContent: 'center',
-    justifyContent: 'center',
-    padding: 10,
-    marginTop: 12,
-  }
 });
