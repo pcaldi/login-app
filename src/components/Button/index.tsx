2321513a0c9321ef26cdd2901f7d8de0f868ed75
pcaldi <pcaldi@gmail.com>
2024-02-12 14:15:31 -0300

    feat: add comentário

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,8 @@
 import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
 
-
+/* Passo a tipagem para o componente, com todas as propriedades do TouchableOpacityProps
+com o Spread Operator {..rest} */
 type ButtonProps = TouchableOpacityProps & {
   title: string;
 
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,7 +2,8 @@
 import { TextInput, TextInputProps } from "react-native";
 import { styles } from "./styles";
 
-
+/* Passo a tipagem para o componente, com todas as propriedades do TextInputProps
+com o Spread Operator {..rest} */
 type Props = TextInputProps & {}
 
 export function Input({ ...rest }: Props) {
