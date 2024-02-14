d9bde7c978444f0e98baf17dc160aed042da59d9
pcaldi <pcaldi@gmail.com>
2024-02-14 18:15:11 -0300

    feat: criado bottomTabs

---
+++ src/routes/bottomTab.tsx
@@ -0,0 +1,38 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+// Telas
+import { Home } from "@/screen/Home";
+import { Profile } from "@/screen/Profile";
+
+
+export function BottomTab() {
+  const { Navigator, Screen } = createBottomTabNavigator();
+
+  return (
+    <Navigator screenOptions={{
+      headerShown: false, tabBarShowLabel: false,
+      tabBarActiveTintColor: '#6c7fd8',
+      tabBarInactiveTintColor: '#c7c7c7',
+    }}>
+      <Screen
+        name="home"
+        component={Home} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }} />
+
+      <Screen
+        name="profile"
+        component={Profile} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }} />
+
+
+    </Navigator>
+  )
+}
