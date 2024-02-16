5f05e260787636885ab47a8076f6a004e98ce3e5
pcaldi <pcaldi@gmail.com>
2024-02-15 21:20:51 -0300

    feat: passando parâmetros para rota

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,12 +1,22 @@
+// Exportando declaração global para adicionar tipos às rotas do React Navigation
 export declare global {
+  // Definindo um namespace ReactNavigation para conter os tipos relacionados à navegação
   namespace ReactNavigation {
+    // Definindo a interface RootParamList para conter os tipos de parâmetros de rota
     interface RootParamList {
+      // Rotas que não esperam nenhum parâmetro
       newUser: undefined;
       recoverPassword: undefined;
       login: undefined;
       home: undefined;
       verifyKey: undefined;
-      userDetails: undefined;
+
+      // Rota userDetails que espera o parâmetro userId do tipo string
+      userDetails: {
+        userId: string;
+      };
+
+      // Rotas que não esperam nenhum parâmetro
       listUser: undefined;
       profile: undefined;
     }
