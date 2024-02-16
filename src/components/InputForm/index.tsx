890f4f40e9e38d9749feeefa9347bac77198c030
pcaldi <pcaldi@gmail.com>
2024-02-16 17:44:13 -0300

    feat: Criando o componente InputForm

---
+++ src/components/InputForm/index.tsx
@@ -0,0 +1,14 @@
+
+import { TextInput, TextInputProps } from "react-native";
+import { styles } from "./styles";
+
+/* Passo a tipagem para o componente, com todas as propriedades do TextInputProps
+com o Spread Operator {..rest} */
+type Props = TextInputProps & {}
+
+export function InputForm({ ...rest }: Props) {
+  return (
+    <TextInput style={styles.input} {...rest} cursorColor='#6c7fd8' />
+
+  )
+}
---
+++ src/components/InputForm/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  input: {
+    width: '90%',
+    backgroundColor: 'transparent',
+    color: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 12,
+    marginTop: 12,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#C7C7C7',
+    fontSize: 18,
+  }
+
+})
