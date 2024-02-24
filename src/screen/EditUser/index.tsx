9185fbb3da0c5a0dea29960920ab73a9add3bfb5
pcaldi <pcaldi@gmail.com>
2024-02-23 23:07:10 -0300

    feat: :sparkles: criado a função handleEditUser para editar o usuário

--- src/screen/EditUser/index.tsx
+++ src/screen/EditUser/index.tsx
@@ -1,6 +1,6 @@
 
 //useState - Armazenar estados
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Incluir os componentes utilizado para estruturar o conteúdo
 import { Alert, ScrollView, Text, View } from 'react-native';
@@ -18,7 +18,7 @@ import api from '@/services/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 //useNavigation - Navegação entre as telas
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 // Componentes
 import { Header } from '@/components/Header';
@@ -29,58 +29,120 @@ import { Loading } from '@/components/Loading';
 // Arquivo com validação do formulário
 import { validateSchemaEdit } from '@/utils/validateSchema';
 
+// Definindo o tipo esperado para os parâmetros da rota
+import { RouteParamsProp } from '../UserDetails';
+
 // Criar e exportar a função com a tela home
 export function EditUser() {
 
   // Armazenar informações nos estados/State
+  const [id, setId] = useState('');
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-  const [situationId, setSituationId] = useState(4);
+  const [situationId, setSituationId] = useState('');
   const [loading, setLoading] = useState(false);
 
+  // Navega entre as telas
   const navigation = useNavigation();
 
+  // Obtendo a rota atual usando o hook useRoute
+  const route = useRoute();
+
+  // Extraindo o parâmetro userId da rota e indicando ao TypeScript o tipo esperado usando as RouteParamsProp
+  const { userId } = route.params as RouteParamsProp
+
+
   // Processar/submeter os dados do formulário
+  const getUser = async () => {
+
+    // Alterar para TRUE e apresentar loading
+    setLoading(true);
+
+    // Recuperar o token
+    const token = await AsyncStorage.getItem('@token')
+
+
+    // Requisição para a API indicando a rota e os dados
+    await api.get(`/users/${userId}`, {
+      'headers': {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then((response) => { // Acessar o then quando a API retornar status sucesso
+
+        //console.log(response.data);
+
+        setId(response.data.user.id);
+        setName(response.data.user.name);
+        setEmail(response.data.user.email);
+        setSituationId(response.data.user.situationId);
+
+
+        // Redirecionar para tela listar após editar
+        //navigation.navigate('listUser');
+
+      }).catch((err) => { // Acessar o catch quando a API retornar status erro
+
+        if (err.response) { // Acessa o IF quando a API retornar erro
+          Alert.alert("Ops", err.response.data.message.toString());
+        } else { // Acessa o ELSE quando a API não responder
+          Alert.alert("Ops", "Erro: Usuário não editado, tente mais tarde!");
+        }
+
+      }).finally(() => {
+
+        // Alterar para FALSE e retira loading
+        setLoading(false);
+      })
+
+
+  }
+
+  // Executar quando carregar a tela e chamar a função getUser();
+  useEffect(() => {
+    getUser();
+  }, []);
+
+
   const handleEditUser = async () => {
 
-    // Usar try e catch para gerenciar exceção/erro
+    // Utilizo o try/catch para gerenciar exceção/erro
     try { // Permanece no try se não houver nenhum erro
 
       // Alterar para TRUE e apresentar loading
       setLoading(true);
 
-      // Recuperar o token
-      const token = await AsyncStorage.getItem('@token')
-
-      // Validar o formulário com Yup
+      // Validar o formulário com YUP
       await validateSchemaEdit.validate({ name, email }, { abortEarly: false });
 
+      // Recuperar o token
+      const token = await AsyncStorage.getItem('@token');
+
       // Requisição para a API indicando a rota e os dados
-      await api.put('/users/', { name, email, situationId }, {
+      await api.put('/users', { id, name, email, situationId }, {
         'headers': {
           'Authorization': `Bearer ${token}`
+
         }
       })
-        .then((response) => { // Acessar o then quando a API retornar status sucesso
+        .then((response) => { // Acessa o then quando a API retornar sucesso
+          Alert.alert('Sucesso', response.data.message)
 
-          Alert.alert("Sucesso", response.data.message);
+          // Redirecionar para tela listar após editar
+          navigation.navigate('userDetails', { userId: id });
+        })
+        .catch((error) => {// Acessa o catch quando a API retornar erro
 
-          // Redirecionar para página detalhes do usuário após a edição
-          //navigation.navigate('userDetails', { userId: user.id });
-          //navigation.goBack();
+          if (error.response) { // Acessa o if quando API retornar erro
+            Alert.alert("Ops", error.response.data.message)
 
-        }).catch((err) => { // Acessar o catch quando a API retornar status erro
-
-          if (err.response) { // Acessa o IF quando a API retornar erro
-            Alert.alert("Ops", err.response.data.message.toString());
           } else { // Acessa o ELSE quando a API não responder
-            Alert.alert("Ops", "Erro: Usuário não editado, tente mais tarde!");
+            Alert.alert("Ops", "Erro: Usuário não editado, tente mais tarde!")
           }
+        })
 
-        });
 
-    } catch (error) { // Acessa o catch quando houver erro no try
-
+    } catch (error) { // Acesso o catch quando houver erro no try
       if (error instanceof yup.ValidationError) { // Acessa o IF quando existir a mensagem de erro
         Alert.alert("Ops", error.errors[0]);
       } else { // Acessa o ELSE quando não existir a mensagem de erro
@@ -88,20 +150,22 @@ export function EditUser() {
       }
     } finally {
 
-      // Alterar para false e ocultar loading
+      // Alterar para False e retira loading
       setLoading(false);
+
     }
   }
-
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <View style={styles.container}>
+
         <Header
           title='Editar usuário'
           IconName='page-first'
           onPress={() => { navigation.goBack() }}
         />
+
         <View style={styles.formContainer}>
           <View style={styles.formLabel}>
 
@@ -144,7 +208,7 @@ export function EditUser() {
         <View style={styles.btnView}>
 
           <Button
-            title='Editar'
+            title='Salvar'
             iconName='account-edit-outline'
             disabled={loading}
             onPress={handleEditUser}
