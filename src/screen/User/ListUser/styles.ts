409be684f4a6e07d4ab0b4ef65a9e77c98d41298
pcaldi <pcaldi@gmail.com>
2024-02-27 22:01:19 -0300

    feat: adicionando style do pagination

--- src/screen/User/ListUser/styles.ts
+++ src/screen/User/ListUser/styles.ts
@@ -18,16 +18,43 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start'
   },
+
   infoData: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
   },
+
   valueData: {
     fontSize: 16,
     color: '#F5F5F5',
     flex: 0
   },
-  icon: {}
+
+  pageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    gap: 6,
+  },
+
+  pageSelected: {
+    backgroundColor: '#c7C7C7',
+    borderWidth: 1,
+    borderColor: '#10101c',
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+  pageTextSelected: {
+    fontSize: 18,
+    padding: 12,
+    fontWeight: '700',
+    color: '#10101c',
+    textAlign: 'center'
+  },
+
 });
